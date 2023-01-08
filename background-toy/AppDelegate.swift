01851bb870b34c9c70c38688542f74f316353842
뱀 <54323618+viyamKUN@users.noreply.github.com>
2023-01-09 01:51:21 +0900

    공백 라인 제거

--- background-toy/AppDelegate.swift
+++ background-toy/AppDelegate.swift
@@ -10,8 +10,6 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
-
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -24,7 +22,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
-
-
 }
 
