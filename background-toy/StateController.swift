544ce793a8551a5486122bdf5de06bfe46425c08
뱀 <54323618+viyamKUN@users.noreply.github.com>
2023-01-10 01:23:37 +0900

    타이머 업데이터 추가

--- background-toy/StateController.swift
+++ background-toy/StateController.swift
@@ -29,8 +29,15 @@ class StateController {
             currentState = .idle
         }
 
+        // Upddate states by time.
+        updateTimer()
         // TODO: 스테이트 업데이트
         // 클릭, 걷기, idle이 시간 초과인 경우 idle
+    }
+    
+    func updateTimer() {
+        // 타이머가 등록된 애니메이션에 대해서
+        // 타이머를 갱신
     }
     
     func resetEveryTick() {
