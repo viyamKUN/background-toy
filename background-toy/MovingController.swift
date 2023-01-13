eccdc3818c39de197e66ddf5340c022ca5460b8e
뱀 <54323618+viyamKUN@users.noreply.github.com>
2023-01-14 03:40:12 +0900

    Position updater (#4)
    * Moving Controller 구현
    
    * 걷기 상태로 전환
    
    * State 전환에 우선순위 변경
    
    * 프레임 간격 좁힘
    
    * 걷지 않는 문제 해결

---
+++ background-toy/MovingController.swift
@@ -0,0 +1,48 @@
+//
+//  MovingController.swift
+//  background-toy
+//
+//  Created by 윤하연 on 2023/01/13.
+//
+
+import Foundation
+import Cocoa
+
+class MovingController {
+    var direction = NSPoint(x: 1, y: 1)
+    
+    func updatePosition(window: NSWindow?, stateController: StateController) {
+        if window === nil {
+            return
+        }
+        let origin = window!.frame.origin
+        let originPoint = NSPoint(x: origin.x, y: origin.y)
+        var desiredPosition = originPoint
+        
+        let isWalking = stateController.currentState == .walk
+        if isWalking {
+            if stateController.isUpdated {
+                updateDirection()
+            }
+            desiredPosition = NSPoint(x: originPoint.x + direction.x,
+                                      y: originPoint.y + direction.y)
+        }
+        
+        window!.setFrameOrigin(desiredPosition)
+    }
+    
+    // Decide randomic direction to walk
+    private func updateDirection() {
+        var x = Int.random(in: -1...1)
+        var y = Int.random(in: -1...1)
+        if x == 0 && y == 0 {
+            if Bool.random() {
+                x = Bool.random() ? -1 : 1
+            }
+            else {
+                y = Bool.random() ? -1 : 1
+            }
+        }
+        direction = NSPoint(x: x, y: y)
+    }
+}
