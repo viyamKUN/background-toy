4c84857aaeda60a759b329d98ccf579191b9e2dd
뱀 <54323618+viyamKUN@users.noreply.github.com>
2023-01-15 18:30:02 +0900

    set app to be always on top

--- background-toy/ViewController.swift
+++ background-toy/ViewController.swift
@@ -49,6 +49,11 @@ class ViewController: NSViewController {
             repeats: true)
         RunLoop.main.add(timer, forMode: RunLoop.Mode.common)
     }
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        view.window?.level = .floating
+    }
 
     override var representedObject: Any? {
         didSet {
