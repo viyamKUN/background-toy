ee881a732a4f33bb47e5f3b87f3417be22553d11
뱀 <54323618+viyamKUN@users.noreply.github.com>
2023-01-15 18:00:51 +0900

    Fix image view (#5)
    * animation flickering 수정
    
    * 방향에 따라서 Flip되도록 수정

---
+++ background-toy/Extensions/NSImageExtension.swift
@@ -0,0 +1,30 @@
+//
+//  NSImageExtension.swift
+//  background-toy
+//
+//  Created by 윤하연 on 2023/01/15.
+//
+
+import Foundation
+import Cocoa
+
+extension NSImage {
+    func flipped(flipHorizontally: Bool = false, flipVertically: Bool = false) -> NSImage {
+        let flippedImage = NSImage(size: size)
+
+        flippedImage.lockFocus()
+
+        NSGraphicsContext.current?.imageInterpolation = .high
+
+        let transform = NSAffineTransform()
+        transform.translateX(by: flipHorizontally ? size.width : 0, yBy: flipVertically ? size.height : 0)
+        transform.scaleX(by: flipHorizontally ? -1 : 1, yBy: flipVertically ? -1 : 1)
+        transform.concat()
+
+        draw(at: .zero, from: NSRect(origin: .zero, size: size), operation: .sourceOver, fraction: 1)
+
+        flippedImage.unlockFocus()
+
+        return flippedImage
+    }
+}
